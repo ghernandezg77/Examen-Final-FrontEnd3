4fbd73450a2bfeb2d79451741a6a4d8f63477352
Alejandra Coronado <aleja.r1231@gmail.com>
2023-09-13 21:08:34 -0500

    Ajuste error dependencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home"
 import Dentist from "./Routes/Detail"
 import Contact from "./Routes/Contact"
