22fa705252dce2c369b83ff1b2d0a626db306fdb
GERMAN <ghernandezg77@gmail.com>
2023-09-14 12:11:58 -0500

    Acutalizacion NavBar y Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import styles from "../Styles/footer.module.css";
+import styles2 from "../Styles/app.module.css";
+import facebook from "/images/ico-facebook.png";
+import instagram from "/images/ico-instagram.png";
+import whatsapp from "/images/ico-whatsapp.png";
+import tiktok from "/images/ico-tiktok.png";
+import { useContext } from "react";
+import { AppContext } from "../Context/AppContext";
+
+
 
 const Footer = () => {
-  return (
-    <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
-    </footer>
-  )
-}
+  const { theme } = useContext(AppContext);
 
-export default Footer
+  return (
+    <main className={`${styles2[theme]}`}>
+      <footer className={styles.footer}>
+        <div className={styles2.icons}>
+          <img src={facebook} alt="facebook" className={styles.icon} />
+          <img src={instagram} alt="instagram" className={styles.icon} />
+          <img src={whatsapp} alt="whatsapp" className={styles.icon} />
+          <img src={tiktok} alt="tiktok" className={styles.icon} />
+        </div>
+        <p>Powered by</p>
+        <img src="/public/images/DH.png" alt='DH-logo' />
+      </footer>
+    </main>
+  );
+};
+
+export default Footer;
